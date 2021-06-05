9cf0f5397aa95173dfcf8fa672ce5784a1170a3d
mani <mgmaddala23@gmail.com>
2021-06-05 11:13:31 +0530

    Final commit by GitA

---
+++ src/test/java/qamm/Mavenjava/demo.java
@@ -0,0 +1,26 @@
+package qamm.Mavenjava;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class demo 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void tes2()
+    {
+        System.out.println("Test pass 2");
+    }
+}
